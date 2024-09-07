4a07f569fdf190b40f2c0785a0898e9e1c0024e1
IsmailDX <ismaildxx@gmail.com>
2024-09-07 18:24:16 +0400

    authentication works

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -5,6 +5,9 @@ import Input from "@/app/components/inputs/Input";
 import Button from "@/app/components/Button";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -37,22 +40,45 @@ const AuthForm = () => {
 
     if (variant === "REGISTER") {
       // Axious register
+      axios
+        .post("/api/register", data)
+        .catch(() => toast.error("Something went wrong"))
+        .finally(() => setIsLoading(false));
     }
     if (variant === "LOGIN") {
       // Next auth sign in
+      signIn("credentials", {
+        ...data,
+        redirect: false,
+      })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error("Invalid credentials");
+          }
+
+          if (callback?.ok && !callback?.error) {
+            toast.success("Logged in!");
+          }
+        })
+        .finally(() => setIsLoading(false));
     }
-
-    const socialAction = (action: string) => {
-      setIsLoading(true);
-
-      //NextAuth social sign in
-    };
   };
 
   const socialAction = (action: string) => {
     setIsLoading(true);
-
     //NextAuth social sign in
+
+    signIn(action, { redirect: false })
+      .then((callback) => {
+        if (callback?.error) {
+          toast.error("Invalid credentials");
+        }
+
+        if (callback?.ok && !callback?.error) {
+          toast.success("Logged in!");
+        }
+      })
+      .finally(() => setIsLoading(false));
   };
 
   return (
---
+++ app/api/register/route.ts
@@ -0,0 +1,32 @@
+import bcrypt from "bcrypt";
+import prisma from "@/app/libs/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { name, email, password } = body;
+
+    if (!name || !email || !password) {
+      return new NextResponse("Name, email and password are required", {
+        status: 400,
+      });
+    }
+
+    //we create hashpassword
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        hashedPassword,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error: any) {
+    console.log(error, "REGISTRATION ERROR");
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
